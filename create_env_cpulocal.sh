0dab10a4733809bcb8419b6f5c0a584d2ad11ca8
nanliha <nanlihao@gmail.com>
2018-06-01 20:03:49 +1000

    switch to pytorch

--- create_env_cpulocal.sh
+++
@@ -1,6 +0,0 @@
-#test on python 3.5.1
-virtualenv --system-site-packages -p python3 myenv && \
-source myenv/bin/activate && \
-pip3 install --ignore-installed --upgrade \
-https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.7.0rc1-cp34-cp34m-linux_x86_64.whl && \
-pip3 install -r requirements.txt
--- create_env_gpuserver.sh
+++
@@ -1,7 +0,0 @@
-module load python/3.5.1 && \
-module load cuda/9.1.85 && \
-module load openmpi-gcc/3.0.0-cuda && \
-virtualenv --system-site-packages -p python3 myenv && \
-source myenv/bin/activate && \
-pip3 install --ignore-installed --upgrade /usr/local/tensorflow/v100/tensorflow-1.7.0rc1-cp35-cp35m-linux_x86_64.whl && \
-pip3 install -r requirements.txt
